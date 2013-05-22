dae7bc56b083a02efdefc93cf0e6e401f0d5e075
root <root@debianLP2013.localdomain>
2013-05-22 14:22:30 +0200

    dero

--- src/Metinet/Bundle/FacebookBundle/Controller/DefaultController.php
+++ src/Metinet/Bundle/FacebookBundle/Controller/DefaultController.php
@@ -14,29 +14,17 @@ class DefaultController extends Controller {
      */
     public function indexAction() {
         
+        $userRepository = $this->getDoctrine()->getRepository('MetinetFacebookBundle:User');
+	$quizzRepository = $this->getDoctrine()->getRepository('MetinetFacebookBundle:Quizz');
         
-        /*$fbConfig = array(
-            'appId' => '575560672464968',
-            'secret' => '73d59b1c294dc995866d5826be80451c'
+        $quizzEnAvant = $quizzRepository->getPromotedQuizz(1);
+        $derniersQuizz = $quizzRepository->getLastQuizz(4);
+        
+        
+        return array(
+            "quizzEnAvant"   => $quizzEnAvant,
+            "derniersQuizz"   => $derniersQuizz
         );
-
-        //$facebook = new Facebook($fbConfig);
-        //$user = $facebook->getUser();
-        $user = $this->container->get('metinet.manager.fbuser')->getUser();
-        if ($user) {
-            try {
-                $attachment = array('message' => 'Le message qui va apparaître sur le Mur',
-                    'name' => 'Le nom de ton post si tu veux le retrouver plus tard!',
-                    'caption' => 'Le titre de ton post sur le Mur',
-                    'picture' => 'http://www.monsite.com/image.gif');
-
-                $results = $this->container->get('metinet.manager.fbuser')->api('/100000481617990/feed/', 'post', $attachment);
-            } catch (FacebookApiException $e) {
-                echo $e->getMessage();
-                $user = null;
-            }
-        }*/
-        return array('highscore' => true);
     }
 
 }
